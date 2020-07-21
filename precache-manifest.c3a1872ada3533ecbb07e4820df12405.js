8759141b435b6cb7782f7b4154e2d7b2045f702b
abdullah-sheikh-au5 <abdullah8shk@gmail.com>
2020-07-21 12:36:29 +0530

    Updates

---
+++ precache-manifest.c3a1872ada3533ecbb07e4820df12405.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3039c18af3e2fe7b1ba7db99f7391b55",
+    "url": "/zoho-books/index.html"
+  },
+  {
+    "revision": "1d03872da1f06496b317",
+    "url": "/zoho-books/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "6ee857697ca582f18549",
+    "url": "/zoho-books/static/js/2.ba0c5d93.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/zoho-books/static/js/2.ba0c5d93.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1d03872da1f06496b317",
+    "url": "/zoho-books/static/js/main.04e3a49c.chunk.js"
+  },
+  {
+    "revision": "c3493dc88ad7e6404830",
+    "url": "/zoho-books/static/js/runtime-main.b91aa810.js"
+  }
+]);
